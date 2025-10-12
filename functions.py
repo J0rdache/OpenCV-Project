1be3e983c29fbf6799a2cabddd954d56f31938f8
BuildTools <unconfigured@null.spigotmc.org>
2025-10-12 06:03:14 -0700

    lots of cleanup

---
+++ functions.py
@@ -0,0 +1,69 @@
+import cv2
+
+def initializeCamera(width = 640, height = 480, fps = 12):
+    
+    # Initialize video capture from the default webcam (index 0)
+    cap = cv2.VideoCapture(1)
+
+    # Check if the webcam is opened correctly
+    if not cap.isOpened():
+        raise IOError("Cannot open webcam")
+    
+    # Force resolution to 640x480 and framerate to 12
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+    cap.set(cv2.CAP_PROP_FPS, fps)
+
+    # Get actual width, height, and framerate
+    frameWidth = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    frameHeight = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    frameRate = cap.get(cv2.CAP_PROP_FPS)
+
+    camInfo = {
+        "width": int(frameWidth),
+        "height": int(frameHeight),
+        "fps": int(frameRate)
+    }
+    return cap, camInfo
+
+def findTarget(faces):
+    # Find the largest face from a list of detected faces
+    # Returns the largest face rectangle from a list of faces, or None if no faces are found
+
+    largestFace = None
+    maxArea = 0
+    for (x, y, w, h) in faces:
+        if w * h > maxArea:
+            maxArea = w * h
+            largestFace = (x, y, w, h)
+    return largestFace
+
+def trackTargetFace(faces, lastKnownTarget, tolerance=25):
+    # Find the target face in a new frame based on the last known position.
+    # Returns the updated face, or none if it is not found
+    # The tolerance is the maximum horizontal and vertical distance the new face  can be from the old one in a new frame
+    (lx, ly, _, _) = lastKnownTarget
+    for (x, y, w, h) in faces:
+        if (x > lx - tolerance and x < lx + tolerance) and (y > ly - tolerance and y < ly + tolerance):
+            return (x, y, w, h)
+    return None
+
+def drawOnFrame(frame, faces, targetFace, camInfo):
+    # Draws rectangles around faces, displays camera info, and displays the horizontal coordinates of the target face
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    fontScale = 0.85
+    thickness = 2
+
+    for (x, y, w, h) in faces:
+        if targetFace is not None and (x, y, w, h) == targetFace:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), thickness)
+            cv2.putText(frame, "Target", (x, y - 10), font, fontScale, (0, 255, 0), thickness)
+            targetXText = f"Target X: {x + w // 2}"
+            cv2.putText(frame, targetXText, (10, camInfo['height'] - 40), font, fontScale, (255, 0, 0), thickness)
+        else:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), thickness)
+    
+    camText = f"{camInfo['width']}x{camInfo['height']} @ {camInfo['fps']} FPS"
+    cv2.putText(frame, camText, (10, 30), font, fontScale, (0, 0, 255), thickness)
+
+    return frame
---
+++ main.py
@@ -0,0 +1,56 @@
+import cv2
+import time
+import functions as fn
+
+def main():
+    # Load the Haar Cascade classifier
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+
+    # initialize the camera
+    cap, camInfo = fn.initializeCamera()
+
+    targetFace = None
+    lastPrintTime = time.time()
+
+    while True:
+        # Read a single frame from the webcam
+        ret, frame = cap.read()
+        # If the frame was not captured successfully, break the loop
+        if not ret:
+            break
+        # Convert the captured frame to grayscale
+        # The face detection algorithm works on grayscale images
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # Detect faces in the grayscale frame
+        # This returns a list of rectangles (x, y, width, height) for each detected face
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+        
+
+        if targetFace is None:
+            # Find the largest face in frame to choose as the target face
+            targetFace = fn.findTarget(faces)
+        else:
+            # Attempt to find the target face in a new frame
+            updatedFace = fn.trackTargetFace(faces, targetFace)
+            if updatedFace is not None:
+                targetFace = updatedFace
+            else:
+                # If the target face was not found again, a new face must be found next frame
+                targetFace = None
+        
+        frame = fn.drawOnFrame(frame, faces, targetFace, camInfo)
+        cv2.imshow('USRT Face Tracking', frame)
+
+        currentTime = time.time()
+        if targetFace and (currentTime - lastPrintTime >= 0.1):
+            (x, y, w, h) = targetFace
+            print(f"Target Center: {x + w // 2}")
+            lastPrintTime = currentTime
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
