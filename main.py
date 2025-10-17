6e41127d96761cee3f46537385dfed0a0a7c32ed
BuildTools <unconfigured@null.spigotmc.org>
2025-10-16 17:46:27 -0700

    It is beautiful

--- main.py
+++ main.py
@@ -1,80 +1,71 @@
-import cv2
+import usrtFaceTracking as ft
+import servoController as sc
+import queue
+import threading
 import time
-import functions as fn
+
+# (Pixels)
+CAMERA_WIDTH = 320
+# (Pixels)
+CAMERA_HEIGHT = 240
+# (Frames per second)
+CAMERA_FPS = 8
+# (N)
+CAMERA_INDEX = 1
+# (File containing cascade classifier)
+CASCADE_CLASSIFIER = 'haarcascade_frontalface_alt.xml'
+# (multiplier greater than 1)
+SCALE_FACTOR = 1.2
+# (N)
+MIN_NEIGHBORS = 2
+# (Pixels, Pixels)
+MIN_SIZE = (1, 1)
+# (Multiplier)
+MOTION_TOLERANCE = 0.75
+# (Seconds)
+TRACKING_GRACE = 2
+# (Number of frames)
+ROLLING_AVG_COUNT = 5
+# (Pixels)
+CENTER_WIDTH = 10
+# (N)
+SERVO_PIN = 15
+# (Pulse width ms)
+SERVO_MIN = 0.5
+# (Pulse width ms)
+SERVO_MAX = 2.5
+# (Degrees per second)
+SPEED = 30
+
+fifoQueue = queue.Queue()
+
+def servo_thread(servo):
+    while True:
+        try:
+            status = fifoQueue.get(block=False)
+            if status == 0:
+                break
+            servo.updateStatus(status)
+        except queue.Empty:
+            pass
+        
+        servo.runServoLoop()
+        time.sleep(0.02)
 
 def main():
-    # Load the Haar Cascade classifier
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
-
-    # initialize the camera
-    cap, camInfo = fn.initializeCamera(320, 240)
-
-    targetFace = None
-    lastPrintTime = time.time()
-    lastGraceTime = time.time()
-    targetXList = []
-    xListPos = 0
-    
-
+    tracker1 = ft.FaceTracker(CAMERA_WIDTH, CAMERA_HEIGHT, CAMERA_FPS, CAMERA_INDEX, CASCADE_CLASSIFIER, SCALE_FACTOR,
+                              MIN_NEIGHBORS, MIN_SIZE, MOTION_TOLERANCE, TRACKING_GRACE, ROLLING_AVG_COUNT,
+                              CENTER_WIDTH)
+    servo1 = sc.ServoController(SERVO_PIN, SERVO_MIN, SERVO_MAX, SPEED)
+    t_servo = threading.Thread(target=servo_thread, args =(servo1,))
+    t_servo.start()
     while True:
-        # Read a single frame from the webcam
-        ret, frame = cap.read()
-        # If the frame was not captured successfully, break the loop
-        if not ret:
+        status = tracker1.update()
+        fifoQueue.put(status)
+        if status == 0:
             break
-        # Convert the captured frame to grayscale
-        # The face detection algorithm works on grayscale images
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        # Detect faces in the grayscale frame
-        # This returns a list of rectangles (x, y, width, height) for each detected face
-        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=2, minSize=(1, 1))
-        
-
-        if targetFace is None:
-            # Find the largest face in frame to choose as the target face
-            targetFace = fn.findTarget(faces)
-        else:
-            # Attempt to find the target face in a new frame
-            updatedFace = fn.trackTargetFace(faces, targetFace)
-            if updatedFace is not None:
-                targetFace = updatedFace
-                lastGraceTime = time.time()
-            elif time.time() - lastGraceTime > 0.5:
-                # If the target face was not found again, a new face must be found next frame
-                targetFace = None
-        if targetFace is not None:
-            centerX = targetFace[0] + targetFace[2] // 2
-            if len(targetXList) < 10:
-                targetXList.append(centerX)
-                xListPos += 1
-            elif xListPos < 10:
-                targetXList[xListPos] = centerX
-                xListPos += 1
-            else:
-                xListPos = 0
-                targetXList[xListPos] = centerX
-                xListPos += 1
-
-                
-        if len(targetXList) != 0:
-            targetAvgX = sum(targetXList) // len(targetXList)
-        else:
-            targetAvgX = 0
-        
-        frame = fn.drawOnFrame(frame, faces, targetFace, camInfo, targetAvgX)
-        cv2.imshow('USRT Face Tracking', frame)
-        currentTime = time.time()
-        if targetFace and (currentTime - lastPrintTime >= 0.1):
-            (x, y, w, h) = targetFace
-            print(f"Target Center: {targetAvgX}")
-            lastPrintTime = currentTime
-
             
-        
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    cap.release()
-    cv2.destroyAllWindows()
+    t_servo.join()
 
 if __name__ == "__main__":
     main()
--- servoController.py
+++ servoController.py
@@ -1,5 +1,29 @@
 import queue
 import time
 
-def servoWorker():
-    pass
+class ServoController:
+    def __init__(self, servo_pin, minMs, maxMs, speed):
+        self.pin = servo_pin
+        self.minDuty = (minMs * 100) / 20
+        self.maxDuty = (maxMs * 100) / 20
+        self.speed = (speed / 180) * (self.maxDuty - self.minDuty)
+        self.currentDuty = self.minDuty + (self.maxDuty - self.minDuty) / 2
+        self.status = 1
+
+    def updateStatus(self, status):
+        self.status = status
+    
+    def runServoLoop(self):
+        if self.status == 1:
+            self.currentDuty = self.minDuty + (self.maxDuty - self.minDuty) / 2
+        elif self.status == 3:
+            if (self.currentDuty - self.speed * 0.02 > self.minDuty):
+                self.currentDuty -= self.speed * 0.02
+            else:
+                self.currentDuty = self.minDuty
+        elif self.status == 4:
+            if (self.currentDuty + self.speed * 0.02 < self.maxDuty):
+                self.currentDuty += self.speed * 0.02
+            else:
+                self.currentDuty = self.maxDuty
+        print(self.currentDuty)
